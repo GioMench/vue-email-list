72dec3a139c72b412841cbd9d74ff086234a59d8
GioMench <giorgiaglm@gmail.com>
2024-03-19 17:38:43 +0100

    set generateMailArray function

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*Descrizione:
 Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.*/
 
@@ -7,21 +8,32 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            randomMail : null
+            randomMail: []
         }
     },
-    methods:{
+
+    methods: {
         callApi() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail?items=10').then((response) => {
                 console.log(response.data.response);
-                this.randomMail = response.data.response
+                theMail = response.data.response;
+                console.log(theMail)
             })
+        },
+
+        generateMailArray() {
+
+            for (let i = 0; i < 10; i++) {
+                this.randomMail.push(this.theMail);
+
+            }
 
         }
     },
     mounted() {
 
-        this.callApi()
+        this.callApi(),
+        this.generateMailArray()
     }
 
 }).mount('#app')
